9b6f147f3c061acf8161d77954e13fe070d7438b
N3theri9N <nnea5215@gmail.com>
2023-03-03 16:33:09 +0900

    PWA 적용
    Progressive Web Application 적용
    - manifest.json 생성
    - 파비콘 생성
    - SEO 설정
    - next.config.js 데이터 내용 변경

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
+const withPWA = require("next-pwa")({ dest: "public", scope: "/busstop/" });
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
     CRYPTO_KEY: process.env.CRYPTO_KEY,
     DATA_GO_KEY: process.env.DATA_GO_KEY,
+    BUSSTOP_VAPID_KEY: process.env.BUSSTOP_VAPID_KEY,
   },
   i18n,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
