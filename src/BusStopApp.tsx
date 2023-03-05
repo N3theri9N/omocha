ab01227860c05aa858a95c5f94964a524566bac8
N3theri9N <nnea5215@gmail.com>
2023-03-05 16:47:55 +0900

    FCM 연동 완료!!!
    앞으로는 다듬는 작업이 필요하다
    ( 토큰값을 유동적으로 전달하기, 위험한 정보들을 환경변수로 변경 , UI 수정 등등 )

--- src/BusStopApp.tsx
+++ src/BusStopApp.tsx
@@ -1,7 +1,47 @@
 import BusStopLayout from "./component/BusStop/BusStopLayout";
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot } from "recoil";
 
-const BusStopApp: React.FC <{routeId:string}>= ({routeId = ""}) => {
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { useEffect } from "react";
+
+// TODO : 환경변수로 변경.
+const firebaseApp: FirebaseApp = initializeApp({
+  apiKey: "AIzaSyCvg2hs_OBGALO3daVcKdVbJJYnbqm_5XU",
+  authDomain: "react-post-de8f7.firebaseapp.com",
+  databaseURL: "https://react-post-de8f7-default-rtdb.firebaseio.com",
+  projectId: "react-post-de8f7",
+  storageBucket: "react-post-de8f7.appspot.com",
+  messagingSenderId: "915123736953",
+  appId: "1:915123736953:web:748b05a226372a7de71fa8",
+});
+
+const BusStopApp: React.FC<{ routeId: string }> = ({ routeId = "" }) => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const messaging = getMessaging(firebaseApp);
+      getToken(messaging, { vapidKey: process.env.BUSSTOP_VAPID_KEY })
+        .then((currentToken) => {
+          if (currentToken) {
+            // console.log(currentToken);
+          } else {
+            // console.log("NO TOKEN AVAILABLE");
+          }
+        })
+        .catch((err) => {
+          console.log("error occured");
+        });
+      onMessage(messaging, (payload) => {
+        // console.log("RECEIVED", payload);
+        const title = "BusStopAlarm";
+        const options = {
+          body: payload?.notification?.body,
+        };
+        const notification = new Notification(title, options);
+      });
+    }
+  }, []);
+
   return (
     <RecoilRoot>
       <BusStopLayout routeId={routeId} />
--- src/component/BusStop/SelectedStations/SelectedStations.tsx
+++ src/component/BusStop/SelectedStations/SelectedStations.tsx
@@ -11,11 +11,11 @@ const SelectedStations: React.FC = () => {
   // (async function runOneSignal (){
   //   await OneSignal.init({ appId: '3ea19a6b-bb93-4d9d-a3d1-1c30f69051d2', allowLocalhostAsSecureOrigin: true });
   //   OneSignal.showSlidedownPrompt();
-  // })(); 
+  // })();
   //  safari_web_id: "web.onesignal.auto.57017041-c410-4b69-86f6-455278402f0c",
-  
-  const [ alarmStation, setAlarmStation ] = useRecoilState(alarmBusStation);
-  const [ contentsVisible, setContentsVisible ] = useState<boolean>(false);
+
+  const [alarmStation, setAlarmStation] = useRecoilState(alarmBusStation);
+  const [contentsVisible, setContentsVisible] = useState<boolean>(false);
 
   const removeStationHandler = (key: string): void => {
     setAlarmStation((prev) => {
@@ -25,10 +25,9 @@ const SelectedStations: React.FC = () => {
   };
 
   const toggleVisible = () => {
-    setContentsVisible(prev => !prev);
-  }
+    setContentsVisible((prev) => !prev);
+  };
 
-  
   useEffect(() => {
     let interval: NodeJS.Timer;
 
@@ -41,42 +40,26 @@ const SelectedStations: React.FC = () => {
         });
         const promiseResponses = await Promise.all(promises);
 
-        promiseResponses.forEach(async(promise, idx) => {
+        promiseResponses.forEach(async (promise, idx) => {
           const xmlString = await promise.text();
           let XmlNode: any = xmlToJson(new DOMParser().parseFromString(xmlString, "text/xml"));
           let result = XmlNode.response.msgBody?.busArrivalItem.predictTime1 || -1;
-          if(result <= 5 && result > 0){
+          if (result <= 5 && result > 0) {
             const key = params[idx];
             const stat: BusStation | undefined = alarmStation.get(key);
-            if(stat?.routeName && stat?.stationName){
-              
-              console.log(`${stat.routeName} 번 버스가 ${result} 분 후 ${stat.stationName}에 도착합니다.`);
-              
-              // clientID : "f4db68147778bb85d8957f12ebc307b2";
-              // const payload = {
-              //   template_object : {
-              //     "object_type":"text",
-              //     "text":`${stat.routeName} 번 버스가 ${result} 분 후 ${stat.stationName}에 도착합니다.`,
-              //     "link":{
-              //        "web_url":"https://omocha-nine.vercel.app/busstop",
-              //        "mobile_web_url":"https://omocha-nine.vercel.app/busstop"
-              //     }
-              //  }
-              // }
-              // await fetch("https://kapi.kakao.com/v2/api/talk/memo/default/send", {
-              //   method: "POST",
-              //   headers: {
-              //     "Content_Type": "application/x-www-form-urlencoded",
-              //     // "Authentication": `Bearer ${token}`
-              //   },
-              //   body: JSON.stringify(payload),
-              // })
-              removeStationHandler(key);
+            if (stat?.routeName && stat?.stationName) {
+              window.navigator.sendBeacon(
+                "/api/busstop/alarm",
+                JSON.stringify({
+                  token: "fQn3I7mbe1AooFWuR-DSAM:APA91bEVUmIs-Mg3tYki4-kdkqBmtrzC_adq3lvsVU8-sqXkTz0h0n7OaEEQsUZsqLYpgQ7l3brp9tupMgr_LXl3z9tSqj4iQBuv8iVlljNh6wWo1aLPCa98Z8zqXBWuYOzcI3mPASBL",
+                  message: `${stat.routeName} 번 버스가 ${result} 분 후 ${stat.stationName}에 도착합니다.`,
+                })
+              );
 
+              removeStationHandler(key);
             }
           }
         });
-
       }, 30000);
     }
     return () => {
@@ -90,24 +73,24 @@ const SelectedStations: React.FC = () => {
       {alarmSize > 0 && (
         <div className={classes.bottomLayout}>
           <div className={classes.bottomLabel} onClick={toggleVisible}>{`선택된 정류장 : ${alarmStation.size} 개`}</div>
-          {contentsVisible && 
-          <div className={classes.bottomContent}>
-            {[...alarmStation.keys()].map((key) => {
-              const station: BusStation | undefined = alarmStation.get(key);
-              if (station) {
-                return (
-                  <div key={key} className={classes.selectedRow}>
-                    <div>{station.routeName}</div>
-                    <div>{station.stationName}</div>
-                    <div>
-                      <button onClick={() => removeStationHandler(key)}>X</button>
+          {contentsVisible && (
+            <div className={classes.bottomContent}>
+              {[...alarmStation.keys()].map((key) => {
+                const station: BusStation | undefined = alarmStation.get(key);
+                if (station) {
+                  return (
+                    <div key={key} className={classes.selectedRow}>
+                      <div>{station.routeName}</div>
+                      <div>{station.stationName}</div>
+                      <div>
+                        <button onClick={() => removeStationHandler(key)}>X</button>
+                      </div>
                     </div>
-                  </div>
-                );
-              }
-            })}
-          </div>
-}
+                  );
+                }
+              })}
+            </div>
+          )}
         </div>
       )}
     </>
